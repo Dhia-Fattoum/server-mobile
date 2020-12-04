c0a21fc50b9d3ff928d71aedc89fd23f457da2bc
oussamasoui <souioussama86@gmail.com>
2020-12-04 12:44:08 +0100

    conversation by user id fixing

--- routes/conversation.js
+++ routes/conversation.js
@@ -8,7 +8,11 @@ const Op = Sequelize.Op;
 router.get("/myConversation/:userId", async (req, res) => {
   console.log(req.params.userId);
   await Conversation.findAll({
-    include: { model: User, required: true,attributes:["userName","profileImage"] },
+    include: {
+      model: User,
+      required: true,
+      attributes: ["userName", "profileImage"],
+    },
 
     where: { "$Users->UserConversations.userId$": req.params.userId },
   }).then((conversation) => res.json(conversation));
@@ -30,22 +34,38 @@ router.post("/addConversation", async (req, res) => {
       where: { id: userId },
       include: { model: Conversation, required: true },
     });
+   // return res.json(convUser)
+    if (!convUser) {
+      let checkTarget = await User.findByPk(targetId)
+      if (!checkTarget)
+        return res.status(400).send("target err");
+        console.log(789);
+      const conv = await Conversation.create({});
+      const recever = await User.findByPk(targetId);
+      const sender = await User.findByPk(userId);
+      await sender.addConversation(conv);
+      await recever.addConversation(conv);
+      console.log(123);
+      return res.json(conv.id);
+    }
     convUser = convUser.Conversations.map((c) => c.id);
     console.log(convUser);
     const convTarget = await User.findOne({
       where: { id: targetId, "$Conversations.id$": convUser },
       include: { model: Conversation, required: true },
     });
-
+    
     if (!convTarget) {
       const conv = await Conversation.create({});
       const recever = await User.findByPk(targetId);
       const sender = await User.findByPk(userId);
       await sender.addConversation(conv);
       await recever.addConversation(conv);
+      console.log(123);
       return res.json(conv.id);
     } else {
-      return res.json(convTarget.Conversations[0]);
+      
+      return res.json(convTarget.Conversations[0].id);
     }
   } catch (error) {
     console.log(error);
@@ -65,4 +85,7 @@ router.delete("/:id", async (req, res) => {
     .catch((err) => console.log(err));
 });
 
+router.get('/', async (req, res) => {
+  await Conversation.findAll().then((users) => res.json(users))
+})
 module.exports = router;
