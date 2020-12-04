0b76bd4c531564413e3b50590b214b7dc7450d82
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-12-04 13:11:47 +0100

    fixing errors

--- database1/models/conversation.js
+++ database1/models/conversation.js
@@ -10,7 +10,6 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Conversation.belongsToMany(models.User, {
         through: "UserConversations",
-        foreignKey:'userId'
       });
       Conversation.hasMany(models.Message, {
         foreignKey: "conversationId",
