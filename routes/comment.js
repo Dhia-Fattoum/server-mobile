6b780f1b312e1e5e45ef4f9aa635ba1003ee6070
helayounsi <hela.ahmedyounsi@gmail.com>
2020-12-07 09:39:08 +0100

    changes in the routes of comment

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {Comment,Post, User} = require('../database1/models')
+const {Comment, Post, User} = require('../database1/models')
 const Sequelize = require('sequelize');
 const Op=Sequelize.Op
 
